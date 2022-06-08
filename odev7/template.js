8de5687f594a31b170a877f68192840c8b8cc9c0
Tolga Gureli <tolgagureli@outlook.com>
2022-06-08 19:38:45 +0300

    odev7 tarihte bugun kısmı düzenlendi

--- odev7/template.js
+++ odev7/template.js
@@ -48,15 +48,35 @@ function updateClock(){
         var months = ["Ocak", "Sub", "Mart", "Nisan", "May", "Haz", "Tem", "Ağus", "Eylül", "Ekim", "Kasım", "Aralık"];
         var week = ["","Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi", "Pazar"];
         var ids = ["dayname", "month", "daynum", "year", "hour", "minutes", "seconds", "period","title"];
+        // tarihte o gün ne olmuşsa statik olarak dizide tutuluyor. Bilgiler gerçek değildir test amacıyla rasgele yazılmıstır
+        // 
         var history = [
-            "1523 - İsveç Bağımsızlık Savaşı sonucunda İsveç Krallığı bağımsızlığını ilan etti.",
-            " 1808 - Joseph Bonaparte, İspanya Kralı oldu.",
-            "1976 - Boris Volinov ve Vitali Jolobov ekibinden oluşan Soyuz 21 uzay aracı yolculuğuna başladı.",
-            "1985 - Aleksey Pajitnov tarafından tasarlanan Tetris oyunu yayınlandı.",
-           " 2012 - Venüs geçişi olarak adlandırılan astronomik olay gerçekleşti.",
-           "1976 - Boris Volinov ve Vitali Jolobov ekibinden oluşan Soyuz 21 uzay aracı yolculuğuna başladı.",
-           "1985 - Aleksey Pajitnov tarafından tasarlanan Tetris oyunu yayınlandı.",
-          " 2012 - Venüs geçişi olarak adlandırılan astronomik olay gerçekleşti."];
+          "1523 - İsveç Bağımsızlık Savaşı sonucunda İsveç Krallığı bağımsızlığını ilan etti.",
+          " 1808 - Joseph Bonaparte, İspanya Kralı oldu.",
+          "1976 - Boris Volinov ve Vitali Jolobov ekibinden oluşan Soyuz 21 uzay aracı yolculuğuna başladı.",
+          "1985 - Aleksey Pajitnov tarafından tasarlanan Tetris oyunu yayınlandı.",
+         " 2012 - Venüs geçişi olarak adlandırılan astronomik olay gerçekleşti.",
+         "1976 - Boris Volinov ve Vitali Jolobov ekibinden oluşan Soyuz 21 uzay aracı yolculuğuna başladı.",
+         "1985 - Aleksey Pajitnov tarafından tasarlanan Tetris oyunu yayınlandı.",
+        " 2012 - Venüs geçişi olarak adlandırılan astronomik olay gerçekleşti.",
+        "1976 - Boris Volinov ve Vitali Jolobov ekibinden oluşan Soyuz 21 uzay aracı yolculuğuna başladı.",
+        "1985 - Aleksey Pajitnov tarafından tasarlanan Tetris oyunu yayınlandı.",
+       " 2012 - Venüs geçişi olarak adlandırılan astronomik olay gerçekleşti.",
+       "1976 - Boris Volinov ve Vitali Jolobov ekibinden oluşan Soyuz 21 uzay aracı yolculuğuna başladı.",
+       "1985 - Aleksey Pajitnov tarafından tasarlanan Tetris oyunu yayınlandı.",
+      " 2012 - Venüs geçişi olarak adlandırılan astronomik olay gerçekleşti.",
+      "1976 - Boris Volinov ve Vitali Jolobov ekibinden oluşan Soyuz 21 uzay aracı yolculuğuna başladı.",
+      "1985 - Aleksey Pajitnov tarafından tasarlanan Tetris oyunu yayınlandı.",
+     " 2012 - Venüs geçişi olarak adlandırılan astronomik olay gerçekleşti.",
+     "1976 - Boris Volinov ve Vitali Jolobov ekibinden oluşan Soyuz 21 uzay aracı yolculuğuna başladı.",
+     "1985 - Aleksey Pajitnov tarafından tasarlanan Tetris oyunu yayınlandı.",
+    " 2012 - Venüs geçişi olarak adlandırılan astronomik olay gerçekleşti.",
+    "1976 - Boris Volinov ve Vitali Jolobov ekibinden oluşan Soyuz 21 uzay aracı yolculuğuna başladı.",
+    "1985 - Aleksey Pajitnov tarafından tasarlanan Tetris oyunu yayınlandı.",
+   " 2012 - Venüs geçişi olarak adlandırılan astronomik olay gerçekleşti.",
+   "1976 - Boris Volinov ve Vitali Jolobov ekibinden oluşan Soyuz 21 uzay aracı yolculuğuna başladı.",
+   "1985 - Aleksey Pajitnov tarafından tasarlanan Tetris oyunu yayınlandı.",
+  " 2012 - Venüs geçişi olarak adlandırılan astronomik olay gerçekleşti."];
            console.log(history)
         var values = [week[dname], months[mo], dnum , yr, hou.pad(2), min.pad(2), sec.pad(2), pe,history[dnum]];
 
